12e35ca7dd5fc15a585e0efdc41d3cd53bed9f1d
MrMijagi <dawid.bilinski.99@gmail.com>
2020-08-09 15:55:24 +0200

    first commit

---
+++ desktop/src/falling/letters/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package falling.letters.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import falling.letters.game.FallingLettersGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FallingLettersGame(), config);
+	}
+}
